601d1b5b76c919a07e9c0c8ba0a6520ab4c5aba8
BahaaHamdy <bahaahamdi45@gmail.com>
2025-04-12 05:13:20 +0200

    CRM App Code

---
+++ mydb.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host ='localhost',
+    user = 'root',
+    passwd = 'password',
+
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute('CREATE DATABASE codemy')
+
+print('ALL DONE!')
+
+##############################################33
+
+# import pymysql
+
+# connection = pymysql.connect(
+#     host='localhost',
+#     user='root',
+#     password='password',
+#     database='codemy'
+# )
+# print("Connection successful!")
