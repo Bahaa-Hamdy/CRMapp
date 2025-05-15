35fdda96b5c7473d3203f74b7860bf74c42fb7f1
BahaaHamdy <bahaahamdi45@gmail.com>
2025-05-15 11:52:19 +0300

    convert into sqlite

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'codemy',
-        'USER': 'root',
-        'PASSWORD': 'Su8.wia&2',
-        'HOST': '127.0.0.1',
-        'PORT':'3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'codemy',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Su8.wia&2',
+        # 'HOST': '127.0.0.1',
+        # 'PORT':'3306',
     }
 }
 
